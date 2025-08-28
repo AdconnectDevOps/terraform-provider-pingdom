c00eb723268fa91cd8dcbf1d1f55bb385f8acd18
Yevhenii Matusevych <zhenya.matusevich@gmail.com>
2025-08-28 18:13:07 +0300

    fix test and version

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/AdconnectDevOps/terraform-provider-pingdom/pingdom"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
 func main() {
